e19072824492b2f70c6edf145267dc58a50f6276
Kilimanjaro874 <vantan2022.228995@gmail.com>
2023-01-18 03:34:07 +0900

    tmp

--- program/game/scene/gm_scene_play.h
+++ program/game/scene/gm_scene_play.h
@@ -1,27 +1,40 @@
-#pragma once
-#include<memory>
 #include "gm_scene_base.h"
-#include "../gm_actor.h"
-#include "../gm_camera.h"
-#include "../gm_assemble_repository.h"
+#include "../model/gm_module.h"
+#include "../model/gm_robot.h"
+#include "../gm_robot_controller.h"
+#include "../model/gm_shot_taget.h"
+#include "../gm_hit_manager.h"
 
-
-class GmCamera;
 class ScenePlay : public SceneBase {
 public:
 	ScenePlay() {}
 	~ScenePlay();
-	//// ----- member variables ----- //
-	GmCamera* camera_ = nullptr;
-	std::shared_ptr<tol::Actor> actor_;
-	std::shared_ptr<tol::AssemRepo> assem_repo_;
-	//tol::Assemble* test_ = nullptr;
-	// ---- actor data : csv ---- //
-	const std::string robot_actor_ = "Actor_RobotObj_01.csv";
-	//// ----- member functions ----- ////
+	//// ----- メンバ変数 ----- //
+	GmCamera* _camera = nullptr;
+	Robot* _robo = nullptr;
+	RobotCont* _controller = nullptr;
+	Parts* _background = nullptr;
+	Parts* _aim_pos = nullptr;
+	HitManager* _hit_manager;
+
+	// --- Map object --- //
+	std::vector<Parts*> _field_obj_li;	// 
+	std::vector<dxe::Mesh*> _floor_obj_li;	//	床メッシュ取得
+
+	// --- ShotTargetObject --- //
+	std::vector<ShotTarget*> _target_obj_v;
+
+	// --- UI --- //
+	int _sight_UI_gh;						// ロボットのサイト表示
+	
+	// --- flag --- //
+	bool _is_clear = false;
+	float _clear_time = 0;
+
+	//// ----- メンバ関数 ----- ////
 	void initialzie() override;
 	void update(float delta_time) override;
 	void render() override;
-
+	void setTargets();
 };
 
