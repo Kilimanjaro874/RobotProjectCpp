9d7eabae2b7721a909204e357f5a4883a9d3396d
Kilimanjaro874 <vantan2022.228995@gmail.com>
2023-01-22 23:11:24 +0900

    developed : [PhysicsHandler]

--- program/game/gm_physics_handler.cpp
+++ program/game/gm_physics_handler.cpp
@@ -8,7 +8,8 @@ tol::PhysicsHandler::PhysicsHandler(float mass) {
 void tol::PhysicsHandler::update(float delta_time, const tnl::Vector3& vel_dir)
 {
 	tnl::Vector3 pow = pid_vel_cont_->update(delta_time, vel_dir, getVelocity());
-
+	acc_ = pow / mass_;
+	velocity_ += acc_ * delta_time;
 }
 
 
