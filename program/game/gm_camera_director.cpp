6d027c57ebbf167e908e389dd8ef4ba89f672815
Kilimanjaro874 <vantan2022.228995@gmail.com>
2023-01-31 10:26:41 +0900

    tmp

--- program/game/gm_camera_director.cpp
+++ program/game/gm_camera_director.cpp
@@ -1,5 +1,7 @@
 #include "gm_camera_director.h"
 
-void tol::TPSCamDirector::update(float delta_time, GmCamera camera) {
-
+void tol::TPSCamDirector::update(float delta_time, GmCamera camera,
+	std::shared_ptr<Object> target, const tnl::Vector3& tar_offset_pos_,
+	std::shared_ptr<Object> object, const tnl::Vector3& obj_offset_pos_) {
+	
 }
--- program/game/gm_camera_director.h
+++ program/game/gm_camera_director.h
@@ -14,11 +14,14 @@ namespace tol {
 	protected:
 		//// ----- Member variables ------ ////
 		// --- define --- //
+		tnl::Vector3 
 		tnl::Vector3 cam_target_offset_;
 		tnl::Vector3 cam_pos_offset_;
 
 		//// ----- Member functions ----- ////
 	public:
-		virtual void update(float delta_time, GmCamera camera, std::shared_ptr<Object> target, );
+		virtual void update(float delta_time, GmCamera camera, 
+			std::shared_ptr<Object> target, const tnl::Vector3& tar_offset_pos_,
+			std::shared_ptr<Object> object, const tnl::Vector3& obj_offset_pos_);
 	};
 }
